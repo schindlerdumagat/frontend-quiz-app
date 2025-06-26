0dcf908bb636ad0eafee5ef20ab967304b3bdd8e
schindlerwork <schindler@xtendly.com>
2025-06-26 19:57:29 +0800

    Fix: option spacing, subject border, Add: Quiz comments

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -12,17 +12,28 @@ export default function Quiz({ questions, onViewScoreClick }) {
   const [score, setScore] = useState(0);
 
   const currentQuestionNumber = questionIndex + 1;
-  const question = questions[questionIndex].question;
-  const answer = questions[questionIndex].answer;
-  const options = questions[questionIndex].options.map((option, index) => {
-    let state = "";
-    if (showAnswer && selectedOption) {
+  const { question, options, answer } = questions[questionIndex];
+
+  // This will be rerendered to update the state of the options after each user interactions
+  const choices = options.map((option, index) => {
+    let state = ""; 
+    
+    // This will run after selecting an answer and clicking the submit button.
+    if (showAnswer) {
+
+      // This will show the user if his selected answer is correct or wrong.
       if (option === selectedOption) {
         state = option === answer ? "correct" : "error";
       } else {
-        state = option === answer && "answer";
+
+        // This will show the correct answer after submission if the user selected the wrong answer.
+        if (option === answer) {
+          state = "answer"
+        }
       }
     } else {
+
+      // This will show the currently selected option by the user before they click submit.
       if (option === selectedOption) {
         state = "selected";
       }
@@ -34,38 +45,48 @@ export default function Quiz({ questions, onViewScoreClick }) {
           letter={String.fromCharCode(index + 65)}
           state={state}
           onOptionClick={() => handleOptionClick(option)}
-          disabled={showAnswer}
+          disabled={showAnswer} // Disables the options after submissions
         >
           {option}
         </Option>
       </li>
     );
   });
-
+  
+  // Sets the currectly selected option
   function handleOptionClick(option) {
-    setSelectedOption(option);
+      setSelectedOption(option); 
   }
 
   function handleSubmitClick() {
+    // If the user clicked submit without selecting an option, it will show an error message
     if (!selectedOption) {
       setError(true);
       return;
     }
 
+    // Checks if the selected option is the correct answer.
+    // It will increment the score if it is correct.
     if (selectedOption === answer) {
       setScore((prev) => prev + 1);
     }
-
+    
+    // This triggers the evaluation of the selected option and revealing the correct answer
     setShowAnswer(true);
   }
 
+  // This redirects you to the next question or to the results page
   function handleNextQuestionClick() {
-    if (questionIndex < 9) {
+  
+    // This will reset the state of the quiz upon moving on to the next question
+    if (questionIndex < questions.length - 1) {
       setQuestionIndex((prev) => prev + 1);
       setSelectedOption(null);
       setShowAnswer(false);
       setError(false);
     } else {
+
+      // It will redirect you to the results page after finishing the quiz
       onViewScoreClick(score);
     }
   }
@@ -81,10 +102,10 @@ export default function Quiz({ questions, onViewScoreClick }) {
       </div>
       <div className="quiz__option-container">
         <ol className="quiz__options" role="list" type="A">
-          {options}
+          {choices}
         </ol>
         {showAnswer && selectedOption ? (
-          <Button onButtonClick={handleNextQuestionClick}>{questionIndex < 9 ? "Next Question" : "View Score"}</Button>
+          <Button onButtonClick={handleNextQuestionClick}>{questionIndex < questions.length - 1 ? "Next Question" : "View Score"}</Button>
         ) : (
           <Button onButtonClick={handleSubmitClick}>Submit Answer</Button>
         )}
